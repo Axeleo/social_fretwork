0d462a2c4bb866ec4b9190b104ddb1a1b2b09235
Tom <thomasalanwaters@gmail.com>
2018-08-14 15:22:05 +1000

    add embed partial and musos_embed controller

--- app/controllers/muso_embeds_controller.rb
+++ app/controllers/muso_embeds_controller.rb
@@ -1,2 +1,32 @@
 class MusoEmbedsController < ApplicationController
+
+  def new
+    @muso_embeds = MusoEmbed.new
+  end
+
+  def edit
+    @muso_embed = MusoEmbed.find(params[:id])
+  end
+
+  def create    
+    @muso_embed = MusoEmbed.new
+    @muso_embed.url = params[:muso_embeds][:url]
+    if @muso_embed.save
+      redirect_to muso_path(@muso_id)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @muso_embed = MusoEmbed.find(params[:id])
+    @muso_embed.url = params[:url]
+    
+    if @muso_embed.save
+      redirect_to muso_path(@muso_id)
+    else
+      render :edit
+    end
+  end
+
 end
