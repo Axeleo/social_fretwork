f42bef16ff1e340b6fa30c7b24258ba2fbd126ec
Tom <thomasalanwaters@gmail.com>
2018-08-14 16:47:21 +1000

    muso embeds further work

--- app/controllers/muso_embeds_controller.rb
+++ app/controllers/muso_embeds_controller.rb
@@ -8,11 +8,13 @@ class MusoEmbedsController < ApplicationController
     @muso_embed = MusoEmbed.find(params[:id])
   end
 
-  def create    
+  def create
     @muso_embed = MusoEmbed.new
     @muso_embed.url = params[:muso_embeds][:url]
+    @muso_embed.muso = current_muso
+    byebug
     if @muso_embed.save
-      redirect_to muso_embed_path(@muso_id)
+      redirect_to edit_muso_path(current_muso.id)
     else
       render :"musos/show"
     end
