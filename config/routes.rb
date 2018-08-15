5347bf7d4d8bcff773239dfe2868338e135ae44a
Axel <dlaxel@hotmail.com>
2018-08-15 14:09:03 +1000

    working on rejecting applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :job_applications, only: [:create, :delete]
+  resources :job_applications, only: [:create, :destroy]
   resources :muso_imgs
   resources :muso_embeds
   resources :muso_reviews
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   get '/login', to: 'logins#muso_or_host'
   get '/hosts/new', to: 'hosts#new'
 
+  put '/job_applications/:id/reject', to: 'job_applications#reject'
 end
 
