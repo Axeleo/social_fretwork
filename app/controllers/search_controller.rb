a7d5eca863d860855d7edaf7e97d26a55ccabb04
Leah Eramo <leah.eramo@rea-group.com>
2018-08-15 14:59:36 +1000

    search for musos complete

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,22 @@
 class SearchController < ApplicationController
     
     def muso_results
+        @filters = params
         queries= []
         
-        queries << [:search_by_name, params[:name_q]] unless params[:name_q] == ""
-        queries << [:search_by_location, params[:location_q]] unless params[:location_q] == ""
+        queries << [:search_by_name, @filters[:name_q]] unless @filters[:name_q] == ""
+        queries << [:search_by_location, @filters[:location_q]] unless @filters[:location_q] == ""
+        @muso_results = queries.inject(Muso) { |obj, method_and_args| obj.send(*method_and_args) }
 
-        @muso_results = queries.inject(Muso.all) { |obj, method_and_args| obj.send(*method_and_args) }
+        min_price = @filters[:bprice_min] == "" ? 0 : @filters[:bprice_min].to_f
+        max_price = @filters[:bprice_max] == "" ? 9999999 : @filters[:bprice_max].to_f
 
+        @muso_results = @muso_results.where(:base_price => min_price..max_price)
+        
+    end
+
+    def browse_all_musos
+        @muso_results = Muso.all
+        render :muso_results
     end
 end
