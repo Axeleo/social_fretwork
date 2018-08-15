416d5f349b766664f7905f894c60609eaec4bb2c
Axel <dlaxel@hotmail.com>
2018-08-15 11:06:49 +1000

    removed line from routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login/musos', to: 'logins#musos'
   get '/login/hosts', to: 'logins#hosts'
-  get '/login', to: 'logins#muso_or_host’
 
   post '/login/musos', to: 'logins#muso_session'
   post '/login/hosts', to: 'logins#host_session'
