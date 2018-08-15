a2d60441e0633583d1984dfbb7b0c8215ea980f6
Caroline <vc.setiawan@gmail.com>
2018-08-15 12:14:10 +1000

    signup for hosts is working!!

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,21 @@
 class HostsController < ApplicationController
+    def new
+        @host = Host.new
+    end
+
+    def create
+        redirect '/musos' if logged_in?
+        @host = Host.new(host_create_params)
+        if @host.save
+            session[:host_id] = @host.id
+            redirect_to hosts_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def host_create_params
+        params.require(:host).permit(:name, :email, :password)
+    end
 end
