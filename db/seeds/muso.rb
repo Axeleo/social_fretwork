5a1294add850f5baa0651b2cd913359474df1a43
Leah Eramo <leah.eramo@rea-group.com>
2018-08-16 12:46:44 +1000

    image seeds

--- db/seeds/muso.rb
+++ db/seeds/muso.rb
@@ -1,3 +1,8 @@
+tag_options = [
+    "rock","pop","birthdays","lgbti friendly","seniors friendly","metal","ska","indie","twee","accoustic","brass","solo artist","syth","italian","baltic","accordion","weddings","glam","folk","future funk","funk","slap-bass","trance","EDM","techno","electronic","DJ","celtic","kids"
+]
+
+
 musos_info = [
     {
         name: "The Indoor Kites",
@@ -46,11 +51,51 @@ musos_info = [
         bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
         base_price: rand(300) ,
         location: "Melbourne,VIC" 
+    },
+    {
+        name: "Creedence Clearwater Revival",
+        email:"ccr@socialfretwork.com",
+        password: "muso",
+        bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
+        base_price: rand(300) ,
+        location: "Wallan,VIC" 
+    },
+    {
+        name: "Flock of Seagulls",
+        email:"fos@socialfretwork.com",
+        password: "muso",
+        bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
+        base_price: rand(300) ,
+        location: "St Kilda,VIC" 
+    },
+    {
+        name: "The Beatles",
+        email:"tb@socialfretwork.com",
+        password: "muso",
+        bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
+        base_price: rand(300) ,
+        location: "Port Melbourne,VIC" 
+    },
+    {
+        name: "The Offspring",
+        email:"to@socialfretwork.com",
+        password: "muso",
+        bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
+        base_price: rand(300) ,
+        location: "Reservoir,VIC" 
+    },
+    {
+        name: "Vanilla Ice",
+        email:"vi@socialfretwork.com",
+        password: "muso",
+        bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, natus, asperiores hic laudantium nostrum voluptatum nam obcaecati animi quisquam eum vitae dolor ea quibusdam nesciunt? Architecto sunt veniam aliquid numquam." ,
+        base_price: rand(300) ,
+        location: "Bayswater,VIC" 
     }
 ]
 
 musos_info.each do |muso|
-    m = Muso.new(
+    m = Muso.create(
         name: muso[:name],
         email: muso[:email],
         password: muso[:password],
@@ -58,5 +103,10 @@ musos_info.each do |muso|
         base_price: muso[:base_price] ,
         location: muso[:location] 
     )
+    m.tag_list.add tag_options.sample(10)
+    image_src = File.join(Rails.root, "/stock_images/img_#{rand(39)}.jpg")
+    src_file = File.new(image_src)
+    m.avatar = src_file
     m.save
+    
 end
