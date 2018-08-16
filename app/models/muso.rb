f4c27d7dc48b5892995aa57e8d53444bf8dc6c87
Leah Eramo <leah.eramo@rea-group.com>
2018-08-16 10:12:01 +1000

    average calc

--- app/models/muso.rb
+++ app/models/muso.rb
@@ -15,5 +15,9 @@ class Muso < ApplicationRecord
     include PgSearch
     pg_search_scope :search_by_name, :against => [:name]
     pg_search_scope :search_by_location, :against => [:location]
+
+    def average_rating
+        avg(muso_reviews.rating)
+    end
     
 end
